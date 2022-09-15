4df6de924a37b4923e4d42e59d498186b9fd222f
Jose Luis Luis <>
2022-09-15 14:54:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+#preguntas de geografia
+
+nombre=input("Ingrese su nombre: ")
+print("\nHola",nombre, "Bienvenido al juego de trivia sobre geografia, \nResponde las siguientes preguntas escribiendo la letra de la alternativa\ny presionando 'enter' para enviar tu respuesta: ")
+print("\nCONOCIENDO PERU: ")
+
+print("1.-¿En que departamento se encuentra la ciudad de moyobamba?:\na) Lima.\nb) Arequipa.\nc) Ica.\nd) San martin.")
+
+a='Lima'
+b='Arequipa'
+c='Ica'
+d='SanMartin'
+respuesta=input("Digite su respuesta: ")
+
+if(respuesta=='d'):
+    R="CORRECTO"
+else:
+    R="INCORRECTO"
+print(R)
+
+input()
+print("\n2.-¿En que departamento se encuentra la ciudad de camaná?:\na) Tumbes.\nb) Arequipa.\nc) Cuzco.\nd) Loreto.")
+a='Tumbes'
+b='Arequipa'
+c='Cuzco'
+d='Loreto'
+respuesta=input("Digite su respuesta: ")
+
+if(respuesta=='b'):
+    R="CORRECTO"
+else:
+    R="INCORRECTO"
+print(R)
+input()
+print("\n3.-¿En que departamento se encuentra La RESERVA NACIONAL DE CALIPUY ?:\na) Puno.\nb) Ayacucho.\nc) La Libertad.\nd) Huancavelica.")
+a='Puno'
+b='Ayacucho'
+c='La Libertad'
+d='Huancavelica'
+respuesta=input("Digite su respuesta: ")
+
+if(respuesta=='c'):
+    R="CORRECTO"
+else:
+   R="INCORRECTO"
+print(R)
+
+input()
+print("\n4.-¿Qué departamento es conocido como RINCON DE LOS MUERTOS ?:\na) Ayacucho.\nb) Junin.\nc) Apurimac.\nd) Piura.")
+a='Ayacucho'
+b='Junin'
+c='Apurimac'
+d='Piura'
+respuesta=input("Digite su respuesta: ")
+
+if(respuesta=='a'):
+    R="CORRECTO"
+else:
+    R="INCORRECTO"
+print(R)
+input()
+print("\n5.-¿Que departamento limita con BRASIL,COLOMBIA Y ECUADOR ?:\na) Madre de Dios.\nb) Tumbes.\nc) Tacna.\nd) Loreto.")
+a='Madre de Dios'
+b='Tumbes'
+c='Tacna'
+d='Loreto'
+respuesta=input("Digite su respuesta: ")
+
+if(respuesta=='d'):
+    R="CORRECTO"
+else:
+    R="INCORRECTO"
+print(R)
+input()
+
+
+print("................FIN PROGRAMA!...............")
